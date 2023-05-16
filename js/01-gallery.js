77230eff923215d69df546572623acc4c929e66e
MichaelMikhieienko <bsmmmv@gmail.com>
2023-05-16 09:45:29 +0300

    Correct

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function createGalleryItemMarkup({ preview, original, description }) {
+  return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}" />
+      </a>
+    </li>
+  `;
+}
+
+const galleryMarkup = galleryItems.map(createGalleryItemMarkup).join('');
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+
+galleryRef.addEventListener('click', onGalleryItemClick);
+
+function onGalleryItemClick(event) {
+  event.preventDefault();
+  
+  const target = event.target;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+  
+  const originalImageSrc = target.dataset.source;
+  
+  const instance = basicLightbox.create(`
+    <img src="${originalImageSrc}" width="800" height="600" alt="Image">
+  `);
+  
+  instance.show();
+}
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
+function createGalleryMarkup(items) {
+    return items
+      .map(({ preview, original, description }) => {
+        return `
+              <li class="gallery__item">
+                  <a class="gallery__link" href="${preview}">
+                      <img class="gallery__image" src="${original}" alt="${description}" />
+                  </a>
+              </li>
+            `;
+      })
+      .join('');
+  }
+  
+  const galleryMarkup = createGalleryMarkup(galleryItems);
+  
+  const container = document.querySelector('.gallery');
+  container.innerHTML = galleryMarkup;
+  const gallery = new SimpleLightbox('.gallery li a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+  });
+
 console.log(galleryItems);
